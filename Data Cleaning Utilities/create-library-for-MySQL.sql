a3f7422f0f6c9597c04299b7f44914b6466ceca8
Daren Cheng <2012DCheng@gmail.com>
2016-10-02 06:14:25 -0500

    Added and tested contraints and triggers to the database schema,

--- Data Cleaning Utilities/create-library-for-MySQL.sql
+++ Data Cleaning Utilities/create-library-for-MySQL.sql
@@ -2,7 +2,7 @@
 -- SYNTAX:
 --     mysql> SOURCE <filename>
 -- EXAMPLE:
---     mysql> SOURCE C:\Users\Cheng\Downloads\create-library-for-MySQL.sql
+--     mysql> SOURCE C:\Users\Cheng\Documents\Visual Studio 2013\Projects\SQL Library Project\Data Cleaning Utilities\create-library-for-MySQL.sql
 
 
 
@@ -43,10 +43,10 @@ CREATE TABLE BORROWER (
   Ssn VARCHAR(11) NOT NULL,
   Fname VARCHAR(255) NOT NULL,
   Lname VARCHAR(255) NOT NULL,
-  Email VARCHAR(255) NOT NULL,
+  Email VARCHAR(255),
   Address VARCHAR(255) NOT NULL,
-  City VARCHAR(255) NOT NULL,
-  State VARCHAR(2) NOT NULL,
+  City VARCHAR(255),
+  State VARCHAR(2),
   Phone VARCHAR(14),
   CONSTRAINT pk_Borrower PRIMARY KEY (Card_id),
   CONSTRAINT uk_Borrower_Ssn UNIQUE (Ssn)
@@ -73,6 +73,7 @@ CREATE TABLE FINES (
   CONSTRAINT pk_Fines PRIMARY KEY (Loan_id),
   CONSTRAINT fk_Fines_Loan_id FOREIGN KEY (Loan_id) REFERENCES BOOK_LOANS(Loan_id)
 );
+
 
 -- Insert Statements
 -- CSV Files must be in the secure_file directory, which
@@ -105,7 +106,9 @@ IGNORE 1 LINES (Card_id, Ssn,Fname, Lname, Email, Address, City, State, Phone);
 SET foreign_key_checks = 1;
 
 
--- Adding constraint and triggers after loading data into system
+-- Adding state constraints and triggers after loading data into system
+
+-- Database
 DELIMITER //
 CREATE TRIGGER default_Book_Loans_Date_out BEFORE INSERT ON BOOK_LOANS FOR EACH ROW
 BEGIN
@@ -120,6 +123,86 @@ CREATE TRIGGER default_Book_Loans_Due_date BEFORE INSERT ON BOOK_LOANS FOR EACH 
 BEGIN
   IF ISNULL(NEW.Due_date) THEN
     SET NEW.Due_date = DATE_ADD(CURRENT_DATE, INTERVAL 14 DAY);
+  END IF;
+END //
+DELIMITER ;
+
+-- Book checkout
+DELIMITER //
+CREATE TRIGGER book_checkout_available BEFORE INSERT ON BOOK_LOANS FOR EACH ROW
+BEGIN
+  IF (
+    (
+	  SELECT COUNT(*) 
+	  FROM BOOK_LOANS BL
+	  WHERE BL.Isbn = NEW.Isbn AND ISNULL(Date_in)
+    ) 
+	> 0 
+  ) 
+  THEN
+    SIGNAL SQLSTATE '45001' SET MESSAGE_TEXT = 'Book not available.';
+  END IF;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER book_checkout_limit BEFORE INSERT ON BOOK_LOANS FOR EACH ROW
+BEGIN
+  IF (
+    (
+	  SELECT COUNT(*) 
+	  FROM BOOK_LOANS BL
+	  WHERE BL.Card_id = NEW.Card_id AND ISNULL(Date_in)
+    ) 
+	>= 3 
+  ) 
+  THEN
+    SIGNAL SQLSTATE '45002' SET MESSAGE_TEXT = 'Book checkout limit reached.';
+  END IF;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER book_checkout_fines BEFORE INSERT ON BOOK_LOANS FOR EACH ROW
+BEGIN
+  IF (
+    (
+	  SELECT COUNT(*) 
+	  FROM BOOK_LOANS BL, FINES FN
+	  WHERE BL.Card_id = NEW.Card_id AND BL.Loan_id = FN.Loan_id AND FN.PAID = FALSE
+    ) 
+	> 0 
+  ) 
+  THEN
+    SIGNAL SQLSTATE '45003' SET MESSAGE_TEXT = 'Cannot checkout book due to account fines.';
+  END IF;
+END //
+DELIMITER ;
+
+DELIMITER //
+CREATE TRIGGER book_checkout_overdue BEFORE INSERT ON BOOK_LOANS FOR EACH ROW
+BEGIN
+  IF (
+    (
+	  SELECT COUNT(*) 
+	  FROM BOOK_LOANS BL
+	  WHERE BL.Card_id = NEW.Card_id AND BL.Due_date < CURRENT_DATE AND ISNULL(BL.Date_in)
+    ) 
+	> 0 
+  ) 
+  THEN
+    SIGNAL SQLSTATE '45004' SET MESSAGE_TEXT = 'Cannot checkout book due to overdue book.';
+  END IF;
+END //
+DELIMITER ;
+
+-- Book check in 
+DELIMITER //
+CREATE TRIGGER book_check_in_fines AFTER UPDATE ON BOOK_LOANS FOR EACH ROW
+BEGIN
+  IF (NEW.Due_date < NEW.Date_in) THEN
+    INSERT INTO FINES (Loan_id, Fine_amt, Paid)
+	VALUES (NEW.Loan_id, DATEDIFF(NEW.Date_in, NEW.Due_date) * 0.25, FALSE);
   END IF;
 END //
 DELIMITER ;
