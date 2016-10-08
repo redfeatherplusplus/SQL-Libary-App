697d45ad8acc42ed0cf715c8cb5e768914f3eb15
Daren Cheng <2012DCheng@gmail.com>
2016-10-08 17:00:05 -0500

    Trying to add Missing Files again...

---
+++ Library DAOs/SQL/Queries.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_DAOs.SQL
+{
+    public static class Queries
+    {
+        public static string BookSearch(string term, BookSearchType searchType)
+        {
+            switch(searchType)
+            {
+                case (BookSearchType.Both):
+                    return
+                        "select bk.*, au.Name " +
+                        "from book bk, authors au, book_authors ba " +
+                        "where bk.Isbn = ba.Isbn " +
+                        "and ba.Author_id = au.Author_id " +
+                        "and (bk.Title like \"%" + term + "%\" or au.Name like \"%" + term + "%\") ";
+                case (BookSearchType.Title):
+                    return
+                        "select bk.*, au.Name " +
+                        "from book bk, authors au, book_authors ba " +
+                        "where bk.Isbn = ba.Isbn " +
+                        "and ba.Author_id = au.Author_id " +
+                        "and (bk.Title like \"%" + term + "%\") ";
+                case (BookSearchType.Author):
+                    return
+                        "select bk.*, au.Name " +
+                        "from book bk, authors au, book_authors ba " +
+                        "where bk.Isbn = ba.Isbn " +
+                        "and ba.Author_id = au.Author_id " +
+                        "and (au.Name like \"%" + term + "%\") ";
+                default:
+                    throw new ArgumentException("Invalid Book Search Type: " + searchType);
+            }
+        }
+
+        public static string AuthorSearch(string isbn)
+        {
+            return
+                "select au.Name " +
+                "from authors au, book_authors ba " +
+                "where ba.Isbn = \"" + isbn + "\" " +
+                "and ba.Author_id = au.Author_id";
+        }
+    }
+}
---
+++ Library DAOs/SQL/Tables.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library_DAOs.SQL
+{
+    public static class Tables
+    {
+        public class Book
+        {
+            public const string Isbn = "Isbn";
+            public const string Title = "Title";
+            public const string Cover = "Cover";
+            public const string Publisher = "Publisher";
+            public const string Pages = "Pages";
+        }
+
+        public class Authors
+        {
+            public const string Author_id = "Author_id";
+            public const string Name = "Name";
+        }
+
+        public class Book_Authors
+        {
+            public const string Author_id = "Author_id";
+            public const string Isbn = "Isbn";
+        }
+
+        public class Borrower
+        {
+            public const string Card_id = "Card_id";
+            public const string Ssn = "Ssn";
+            public const string Fname = "Fname";
+            public const string Lname = "Lname";
+            public const string Email = "Email";
+            public const string Address = "Address";
+            public const string City = "City";
+            public const string State = "State";
+            public const string Phone = "Phone";
+
+        }
+
+        public class Book_Loans
+        {
+            public const string Loan_id = "Loan_id";
+            public const string Isbn = "Isbn";
+            public const string Card_id = "Card_id";
+            public const string Date_Out = "Date_out";
+            public const string Due_date = "Due_date";
+            public const string Date_in = "Date_in";
+        }
+
+        public class Fines
+        {
+            public const string Loan_id = "Loan_id";
+            public const string Fine_amt = "Fine_amt";
+            public const string Paid = "Paid";
+        }
+    }
+}
